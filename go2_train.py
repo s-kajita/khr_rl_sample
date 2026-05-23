24d9a47968c2a85e53fc4b3a7ead0c494d91845b
Shuuji Kajita <shuuji.kajita@gmail.com>
2026-05-23 16:02:59 +0900

    walk learning based on go2 example

---
+++ go2_train.py
@@ -0,0 +1,183 @@
+import argparse
+import os
+import pickle
+import shutil
+from importlib import metadata
+
+try:
+    if int(metadata.version("rsl-rl-lib").split(".")[0]) < 5:
+        raise ImportError
+except (metadata.PackageNotFoundError, ImportError) as e:
+    raise ImportError("Please install 'rsl-rl-lib>=5.0.0'.") from e
+from rsl_rl.runners import OnPolicyRunner
+
+import genesis as gs
+
+from go2_env import Go2Env
+
+
+def get_train_cfg(exp_name):
+    train_cfg_dict = {
+        "algorithm": {
+            "class_name": "PPO",
+            "clip_param": 0.2,
+            "desired_kl": 0.01,
+            "entropy_coef": 0.01,
+            "gamma": 0.99,
+            "lam": 0.95,
+            "learning_rate": 0.001,
+            "max_grad_norm": 1.0,
+            "num_learning_epochs": 5,
+            "num_mini_batches": 4,
+            "schedule": "adaptive",
+            "use_clipped_value_loss": True,
+            "value_loss_coef": 1.0,
+        },
+        "actor": {
+            "class_name": "MLPModel",
+            "hidden_dims": [512, 256, 128],
+            "activation": "elu",
+            "distribution_cfg": {
+                "class_name": "GaussianDistribution",
+                "init_std": 1.0,
+                "std_type": "scalar",
+            },
+        },
+        "critic": {
+            "class_name": "MLPModel",
+            "hidden_dims": [512, 256, 128],
+            "activation": "elu",
+        },
+        "obs_groups": {
+            "actor": ["policy"],
+            "critic": ["policy"],
+        },
+        "num_steps_per_env": 24,
+        "save_interval": 100,
+        "run_name": exp_name,
+        "logger": "tensorboard",
+    }
+
+    return train_cfg_dict
+
+
+def get_cfgs():
+    env_cfg = {
+        "num_actions": 12,
+        # joint/link names
+        "default_joint_angles": {  # [rad]
+            "l_hip_yaw": 0.0,
+            "l_hip_roll": 0.0,
+            "l_hip_pitch": -0.2,
+            "l_knee_pitch": 0.4,
+            "l_ankle_pitch": -0.2,
+            "l_ankle_roll": 0.0,
+
+            "r_hip_yaw": 0.0,
+            "r_hip_roll": 0.0,
+            "r_hip_pitch": -0.2,
+            "r_knee_pitch": 0.4,
+            "r_ankle_pitch": -0.2,
+            "r_ankle_roll": 0.0,
+        },
+        "joint_names": [
+            "l_hip_yaw",
+            "l_hip_roll",
+            "l_hip_pitch",
+            "l_knee_pitch",
+            "l_ankle_pitch",
+            "l_ankle_roll",
+
+            "r_hip_yaw",
+            "r_hip_roll",
+            "r_hip_pitch",
+            "r_knee_pitch",
+            "r_ankle_pitch",
+            "r_ankle_roll",
+        ],
+        # PD
+        "kp": 25.0,
+        "kd": 0.5,
+
+        "armature": 0.01,   # [kgm^2]  default 0.1
+
+        # termination
+        "termination_if_roll_greater_than": 50,  # degree
+        "termination_if_pitch_greater_than": 50,
+        # base pose
+        "base_init_pos": [0.0, 0.0, 0.28],
+        "base_init_quat": [1.0, 0.0, 0.0, 0.0],
+        "episode_length_s": 20.0,
+        "resampling_time_s": 4.0,
+        "action_scale": 0.25,
+        "simulate_action_latency": True,
+        "clip_actions": 100.0,
+    }
+    obs_cfg = {
+        "obs_scales": {
+            "lin_vel": 2.0,
+            "ang_vel": 0.25,
+            "dof_pos": 1.0,
+            "dof_vel": 0.05,
+        },
+    }
+    reward_cfg = {
+        "tracking_sigma": 0.25,
+        "base_height_target": 0.3,
+        "feet_height_target": 0.075,
+        "reward_scales": {
+            "tracking_lin_vel": 1.0,
+            "tracking_ang_vel": 0.2,
+            "lin_vel_z": -1.0,
+            "base_height": -50.0,
+            "action_rate": -0.005,
+            "similar_to_default": -0.1,
+        },
+    }
+    command_cfg = {
+        "num_commands": 3,
+        "lin_vel_x_range": [0.5, 0.5],
+        "lin_vel_y_range": [0, 0],
+        "ang_vel_range": [0, 0],
+    }
+
+    return env_cfg, obs_cfg, reward_cfg, command_cfg
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="go2-walk")
+    parser.add_argument("-B", "--num_envs", type=int, default=4096)
+    parser.add_argument("-I","--max_iterations", type=int, default=101)
+    parser.add_argument("--seed", type=int, default=1)
+    args = parser.parse_args()
+
+    log_dir = f"logs/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg = get_cfgs()
+    train_cfg = get_train_cfg(args.exp_name)
+
+    if os.path.exists(log_dir):
+        shutil.rmtree(log_dir)
+    os.makedirs(log_dir, exist_ok=True)
+
+    with open(f"{log_dir}/cfgs.pkl", "wb") as f:
+        pickle.dump([env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg], f)
+
+    gs.init(backend=gs.gpu, precision="32", logging_level="warning", seed=args.seed, performance_mode=True)
+
+    env = Go2Env(
+        num_envs=args.num_envs, env_cfg=env_cfg, obs_cfg=obs_cfg, reward_cfg=reward_cfg, command_cfg=command_cfg
+    )
+
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device=gs.device)
+
+    runner.learn(num_learning_iterations=args.max_iterations, init_at_random_ep_len=True)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+# training
+python examples/locomotion/go2_train.py
+"""
